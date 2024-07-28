43d15cb57047dcd5ea47f62304bbb59c5b344ad3
mattdo-dev <domattgmt7@gmail.com>
2024-07-28 04:20:59 -0700

    feat: additional api routing features for public repo modules

---
+++ src/pages/api/github/fetch-commits.ts
@@ -0,0 +1,27 @@
+﻿import { Commit } from "@/app/interface";
+
+export const fetchCommits = async (): Promise<{ commits: Commit[], error: string | null }> => {
+    const API_SECRET_KEY = process.env.API_SECRET_KEY as string;
+
+    try {
+        const response = await fetch(`${process.env.ROOT_URL}/api/github/page`, {
+            headers: {'x-api-key': API_SECRET_KEY},
+        });
+
+        if (!response.ok) throw new Error("Failed to fetch repositories");
+
+        const commits: Commit[] = await response.json();
+
+        return {
+            commits,
+            error: null,
+        };
+    } catch (error) {
+        console.error('Error fetching repositories:', error);
+
+        return {
+            commits: [],
+            error: error instanceof Error ? error.message : 'Unknown error occurred',
+        };
+    }
+};
---
+++ src/pages/api/hello.ts
@@ -0,0 +1,5 @@
+﻿import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({text: 'hello'});
+}
